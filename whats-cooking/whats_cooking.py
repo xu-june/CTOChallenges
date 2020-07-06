6ec1b90b5d235bd0402cd593563d1041c6317cf0
June Xu <dao-jun.xu@accenturefederal.com>
2020-07-06 12:49:47 -0400

    added comments and changed model in whats_cooking.py

--- whats-cooking/whats_cooking.py
+++ whats-cooking/whats_cooking.py
@@ -9,11 +9,13 @@ import sklearn
 from sklearn import model_selection
 from sklearn import preprocessing
 from sklearn import svm
+from sklearn import linear_model
 from sklearn.svm import SVC
 from sklearn.metrics import accuracy_score
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
 from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.linear_model import LogisticRegression
 
 import csv as csv
 import matplotlib.pyplot as plt
@@ -27,41 +29,37 @@ test_path = ".\\test.json"
 train = pd.read_json(train_path)
 test = pd.read_json(test_path)
 
-recipe = [" ,".join(item) for item in train['ingredients']]
-culture = [" ,".join(item) for item in test['ingredients']]
+# Separate training and testing data
+recipe_tr = [" ,".join(item) for item in train['ingredients']]
+recipe_ts = [" ,".join(item) for item in test['ingredients']]
 
-train['recipe'] = recipe
-train['ingredients'] = recipe
+train['recipe'] = recipe_tr
+test['recipe'] = recipe_ts
 
-test['recipe'] = culture
-
-Train_X, Test_X, Train_Y, Test_Y = model_selection.train_test_split(train['recipe'],train['cuisine'],test_size=0.3)
+x_train, x_test, y_train, y_test = model_selection.train_test_split(train['recipe'], train['cuisine'],test_size=0.3)
 
 labeler = LabelEncoder()
 
-num_recipe = labeler.fit_transform(Train_Y)
-num_culture = labeler.fit_transform(Test_Y)
-tfidf = TfidfVectorizer()
-tfidf.fit(recipe)
+# Encode data (like get_dummies) so it can be processed
+num_recipe_tr = labeler.fit_transform(y_train)
+num_recipe_ts = labeler.fit_transform(y_test)
+tfidf = TfidfVectorizer()   # Maps text to numerical value that represents how frequently the text appears in data - like mapping Category values
+tfidf.fit(recipe_tr)
 
-# Train
+# Train using logistic regression model
 np.random.seed(42)
-matrixtfidf = tfidf.transform(Train_X)
-test_sheet = tfidf.transform(Test_X)
-SVM = SVC(C=3, kernel="linear")
-SVM.fit(matrixtfidf, num_recipe)
-prediction = SVM.predict(test_sheet)
-print("Accuracy: ",accuracy_score(prediction, num_culture)*100)
+matrixtfidf = tfidf.transform(x_train)
+test_sheet = tfidf.transform(x_test)
+
+LR = LogisticRegression(C=8.5, solver = 'liblinear')
+LR.fit(matrixtfidf, num_recipe_tr)    # Model is learning the relationship between the descriptors and the cuisine int values - training
+predictions = LR.predict(test_sheet)    # Use info from the training (line above) to predict Category column for x_test - still part of training
+print (accuracy_score(num_recipe_ts, predictions))
 
 # Test
-label = labeler.fit_transform(train['cuisine'])
-cook = tfidf.transform(train['recipe'])
-final_svm = SVC(C=2, kernel='linear')
-final_svm.fit(cook, label)
-
-final_tfidf = tfidf.transform(culture)
-solution = final_svm.predict(final_tfidf)
-cuisine = labeler.inverse_transform(solution)
+final_tfidf = tfidf.transform(recipe_ts)
+test_results = LR.predict(final_tfidf)    # Test for real! Predict cuisines for test.json
+cuisine = labeler.inverse_transform(test_results)
 
 # Export
 submission = pd.DataFrame({'id': test['id'], 'cuisine': cuisine}, columns=['id', 'cuisine'])
